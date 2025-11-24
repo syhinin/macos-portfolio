8639f3db42219aa56bab1bbf82cdc3fe82936a1b
Oleksandr Sukhinin <syhinin@gmail.com>
2025-11-24 09:26:26 +0100

    configure alias imports

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,7 @@
+export const Navbar = () => {
+  return (
+    <nav className="w-full h-16 bg-gray-800 text-white flex items-center px-4">
+      <div className="text-lg font-bold">My Portfolio</div>
+    </nav>
+  );
+};
