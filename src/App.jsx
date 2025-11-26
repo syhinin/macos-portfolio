3b6154a55acae0b56f6d9e49993e3aa7211279cf
Oleksandr Sukhinin <syhinin@gmail.com>
2025-11-26 10:35:53 +0100

    add Welcome component template

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { Navbar } from "@components/Navbar";
+import { Navbar, Welcome } from "@components";
 
 function App() {
   return (
     <>
       <Navbar />
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Welcome />
     </>
   );
 }
---
+++ src/components/index.js
@@ -0,0 +1,2 @@
+export { Navbar } from "./Navbar";
+export { Welcome } from "./Welcome";
