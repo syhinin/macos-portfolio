447addea5206f8cebc6fd73786751d5f5cb46c0a
Oleksandr Sukhinin <syhinin@gmail.com>
2025-11-26 08:58:52 +0100

    add Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,31 @@
+import dayjs from "dayjs";
+
+import { NAV_LINKS, NAV_ICONS } from "@constants";
+
 export const Navbar = () => {
   return (
-    <nav className="w-full h-16 bg-gray-800 text-white flex items-center px-4">
-      <div className="text-lg font-bold">My Portfolio</div>
+    <nav className="">
+      <div >
+        <img src="/images/logo.svg" alt="Logo" />
+        <p className="font-bold">My Portfolio</p>
+        <ul>
+          {NAV_LINKS.map(({ name, id }) => (
+            <li key={id}>
+              <p>{name}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <ul>
+          {NAV_ICONS.map(({ img, id }) => (
+            <li key={id}>
+              <img className="icon-hover" src={img} alt={`icon-${id}`} />
+            </li>
+          ))}
+        </ul>
+        <time>{dayjs().format("ddd MMM D hh:mm")}</time>
+      </div>
     </nav>
   );
 };
